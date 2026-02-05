5d5e38fa544af0782cb18a43eb9ce10d3e1d476a
Apurv <apurvanandtigh632@gmail.com>
2026-02-05 19:42:04 +0530

    First commit

---
+++ D01C01.C
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    int arr[100];
+    int n, pos, x, i;
+
+    scanf("%d" , &n);
+
+    for(int i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &pos);
+    scanf("%d", &x);
+
+    for(i=n; i>=pos; i--){
+        arr[i]=arr[i-1];
+    }
+
+    arr[pos-1]=x;
+    n++;
+
+    for(i=0; i<n; i++){
+        printf("%d", arr[i]);
+    }
+
+    return 0;
+}
